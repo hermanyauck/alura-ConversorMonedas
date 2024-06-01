be29c4b7bba1b033c968a76ae713f5af54dd4653
herman <herman@yauck>
2024-06-01 12:29:00 -0300

    add options

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        int num;
+        Scanner lectura = new Scanner(System.in);
+        while (true) {
+            System.out.println("""
+                    Bienvenidos al conversor de monedas de Herman Yauck
+                                    
+                    Selecciones una de las opciones de conversion:
+                    1) Dolar -> Peso argentino
+                    2) Peso Argentino -> Dolar
+                    3) Euro -> Peso argentino
+                    4) Peso Argentino -> Euro
+                    5) Dolar -> Peso Mexicano
+                    6) Euro -> Real Brasileño
+                    7) Salir
+                    ELIJA UNA OPCION VALIDA: 
+                    """);
+            num = Integer.valueOf(lectura.nextLine());
+            if (num > 0 && num <= 7){
+                break;
+            }
+            System.out.println("OPCION INGRESADA NO ES VALDIA:");
+        }
+        if (num != 7) {
+            System.out.println("Cuantos quiere convertir:");
+            int pesos = Integer.valueOf(lectura.nextLine());
+            //Conexion.conectar();
+        }
     }
 }
